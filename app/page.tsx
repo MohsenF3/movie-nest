b4426558d294bfeb7cd2b0a6ab778a439ab06517
MohsenFa <3mohsenfaramarzi@gmail.com>
2024-12-24 23:10:18 +0330

    feat: :sparkles: complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import HeroSection from "@/components/home/hero-section";
+
+export default function HomePage() {
   return (
-    <div className="">
-      <h1>This is home page</h1>
+    <div className="mb-10 space-y-20">
+      <HeroSection />
     </div>
   );
 }
---
+++ components/home/hero-section.tsx
@@ -0,0 +1,28 @@
+import { FlipWords } from "../ui/flip-words";
+import { HeroSectionIcon } from "./hero-section-icon";
+
+export default function HeroSection() {
+  const words = ["Possibilities", "Stories", "Adventures"];
+
+  return (
+    <section className="flex flex-col items-center md:flex-row">
+      <div className="w-auto shrink-0 md:w-1/3">
+        <HeroSectionIcon />
+      </div>
+
+      <div className="space-y-5">
+        <h1 className="tracking-wide">
+          <span className="mb-1 block">
+            <span className="text-primary">Discover</span>
+            <FlipWords words={words} />
+          </span>
+          <span> Within the World of Cinema</span>
+        </h1>
+        <p>
+          Unleash your curiosity with insights, reviews, and stories that bring
+          movies to life.
+        </p>
+      </div>
+    </section>
+  );
+}
