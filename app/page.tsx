ef454bfcd318fe1ca31780ff7f971e108d4c8e3c
MohsenFa <3mohsenfaramarzi@gmail.com>
2024-12-25 23:49:13 +0330

    un comment hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MovieSection from "@/components/movie-section";
 export default function HomePage() {
   return (
     <div className="mb-10 space-y-20">
-      {/* <HeroSection /> */}
+      <HeroSection />
 
       {/* <MovieSection title="Now Playing" target="now_playing" /> */}
       {/* <MovieSection title="Upcoming" target="upcoming" /> */}
