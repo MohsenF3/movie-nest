f059caee2f618aaf91ae4c074cd26a65620fcb66
MohsenFa <3mohsenfaramarzi@gmail.com>
2024-12-26 00:24:04 +0330

    uncomment movies secrions

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function HomePage() {
     <div className="mb-10 space-y-20">
       <HeroSection />
 
-      {/* <MovieSection title="Now Playing" target="now_playing" /> */}
-      {/* <MovieSection title="Upcoming" target="upcoming" /> */}
-      {/* <MovieSection title="Top Rated" target="top_rated" /> */}
-      {/* <MovieSection title="Popular" target="popular" /> */}
+      <MovieSection title="Now Playing" target="now_playing" />
+      <MovieSection title="Upcoming" target="upcoming" />
+      <MovieSection title="Top Rated" target="top_rated" />
+      <MovieSection title="Popular" target="popular" />
     </div>
   );
 }
